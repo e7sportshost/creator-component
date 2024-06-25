6f7e3d168f3c4ffac3f3b786a88c05f93a113d0c
cannon <cannon>
2024-06-25 17:10:48 +0800

    a

--- Pagination.vue
+++ Pagination.vue
@@ -5,8 +5,8 @@ const props = defineProps({
 })
 </script>
 <template>
-  <div class="m-5" v-if="links.length > 3">
-      <div class="flex flex-wrap -mb-1 justify-end">
+  <div class="m-0" v-if="links.length > 3">
+      <div class="flex flex-wrap -mb-1 justify-start">
           <template v-for="(link, p) in links" :key="p">
               <div v-if="link.url === null" class="mr-1 mb-1 px-4 py-3 text-sm leading-4 text-gray-400 border rounded"
                   v-html="link.label" />
