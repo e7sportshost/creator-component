380c5db1d5447b32027561f4c11d7a3461f55a06
cannon <p12576999@gmail.com>
2024-07-17 17:55:59 +0800

    a

--- Create.vue
+++ Create.vue
@@ -4,18 +4,36 @@ import { Link, usePage } from '@inertiajs/vue3'
 import { ElButton } from 'element-plus';
 import { Plus } from '@element-plus/icons-vue'
 
+const props = defineProps({
+  LinkHref: { type: String, default: null },
+  link: { type: Boolean, default: true },
+})
+
 const page = usePage();
 const prefix = page.props.prefix || 'backend';
 
 </script>
 
 <template>
-  <Link
+  <template
     v-if="route().has(`${ prefix }.${ $page.props.routeNameData }.create`) && ( $page.props.permissions.includes(`create ${ $page.props.routeNameData }`) || $page.props.auth.user.super_admin )"
-    :href="route(`${ prefix }.${ $page.props.routeNameData }.create`)"
   >
-    <ElButton :icon="Plus" type="success" size="large">
-        <div class="hidden sm:block sm:m-0">{{ $page.props.langs.create }}</div>
-    </ElButton>
-  </Link>
+    <Link
+      v-if="link"
+      :href="route(`${ prefix }.${ $page.props.routeNameData }.create`)"
+      class="ml-4"
+    >
+      <ElButton :icon="Plus" type="success" size="large">
+          <div class="hidden sm:block sm:m-0">{{ $page.props.langs.create }}</div>
+      </ElButton>
+    </Link>
+
+    <div
+      v-else
+    >
+      <ElButton :icon="Plus" type="success" size="large">
+          <div class="hidden sm:block sm:m-0">{{ $page.props.langs.create }}</div>
+      </ElButton>
+    </div>
+  </template>
 </template>
--- Edit.vue
+++ Edit.vue
@@ -7,6 +7,7 @@ const props = defineProps({
   permissions: { type: Boolean, default: true },
   LinkHref: { type: String, default: null },
   permissionsData: { type: String, default: null },
+  link: { type: Boolean, default: true },
 })
 
 const page = usePage();
@@ -15,11 +16,26 @@ const prefix = page.props.prefix || 'backend';
 </script>
 
 <template>
+  <template
+    v-if="!permissions || (route().has(`${ prefix }.${ $page.props.routeNameData }.edit`, item.id) && permissions && ($page.props.permissions.includes(`edit ${ permissionsData || $page.props.routeNameData }`) || $page.props.auth.user.super_admin))"
+  >
     <Link
+      v-if="link"
       :href="LinkHref || route(`${ prefix }.${ $page.props.routeNameData }.edit`, item.id)"
-      v-if="!permissions || (route().has(`${ prefix }.${ $page.props.routeNameData }.edit`, item.id) && permissions && ($page.props.permissions.includes(`edit ${ permissionsData || $page.props.routeNameData }`) || $page.props.auth.user.super_admin))"
       class="inline-block pr-4 text-green-400 duration-100 rounded hover:text-green-600"
     >
-      <PencilSquareIcon class="w-6 h-6" />
+      <slot>
+        <PencilSquareIcon class="w-6 h-6" />
+      </slot>
     </Link>
+
+    <div
+      v-else
+      class="inline-block pr-4 text-green-400 duration-100 rounded hover:text-green-600"
+    >
+      <slot>
+        <PencilSquareIcon class="w-6 h-6" />
+      </slot>
+    </div>
+  </template>
 </template>
