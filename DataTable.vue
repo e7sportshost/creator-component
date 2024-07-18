8f968d6dcdc430b3e79f9e805fabc3eb9184d98b
cannon <p12576999@gmail.com>
2024-07-18 17:52:46 +0800

    a

--- DataTable.vue
+++ DataTable.vue
@@ -181,7 +181,7 @@ defineExpose({
         <el-button :icon="Search" class="ml-4" size="large" @click="searchData" type="primary">
             <div class="hidden sm:block sm:m-0">{{ langs.search }}</div>
         </el-button>
-        <Create v-if="createBtn" class="ml-4" />
+        <Create v-if="createBtn" />
     </div>
 
     <template v-if="advanced">
