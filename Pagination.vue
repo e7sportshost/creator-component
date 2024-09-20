c4c0b67f7dac8584a7a0e78df4111834674a69fd
cannon <p12576999@gmail.com>
2024-09-20 16:39:30 +0800

    ;a

--- Pagination.vue
+++ Pagination.vue
@@ -2,6 +2,7 @@
 import { Link } from '@inertiajs/vue3'
 const props = defineProps({
     links: { type: Array },
+    preserveState: { type: Boolean, default: false },
 })
 </script>
 <template>
@@ -11,8 +12,9 @@ const props = defineProps({
               <div v-if="link.url === null" class="mr-1 mb-1 px-4 py-3 text-sm leading-4 text-gray-400 border rounded"
                   v-html="link.label" />
               <Link v-else
-                  class="mr-1 mb-1 px-4 py-3 text-sm leading-4 border rounded  focus:border-indigo-500 focus:text-indigo-500"
-                  :class="{ 'bg-blue-700 text-white': link.active }" :href="link.url" v-html="link.label" />
+                    preserveState
+                    class="mr-1 mb-1 px-4 py-3 text-sm leading-4 border rounded  focus:border-indigo-500 focus:text-indigo-500"
+                    :class="{ 'bg-blue-700 text-white': link.active }" :href="link.url" v-html="link.label" />
           </template>
       </div>
   </div>
