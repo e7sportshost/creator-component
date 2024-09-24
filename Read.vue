14ab261eca45eb593a5febaf8cee98e5364a7b93
cannon <p12576999@gmail.com>
2024-09-24 11:45:53 +0800

    a

--- Read.vue
+++ Read.vue
@@ -8,6 +8,7 @@ const props = defineProps({
   permissions: { type: Boolean, default: true },
   LinkHref: { type: String, default: null },
   permissionsData: { type: String, default: null },
+  link: { type: Boolean, default: true },
 })
 
 const page = usePage();
@@ -16,11 +17,27 @@ const prefix = page.props.prefix || 'backend';
 </script>
 
 <template>
-  <Link
-    :href="LinkHref || route(`${ prefix }.${ $page.props.routeNameData }.show`, item.id)"
+  <template
     v-if="!permissions || (route().has(`${ prefix }.${ $page.props.routeNameData }.show`, item.id) && permissions && ($page.props.permissions.includes(`read ${ permissionsData || $page.props.routeNameData }`) || $page.props.auth.user.super_admin))"
-    class="inline-block pr-4 duration-100 rounded hover:text-green-600"
   >
-    <EyeIcon class="w-6 h-6" />
-  </Link>
+    <Link
+      v-if="link"
+      :href="LinkHref || route(`${ prefix }.${ $page.props.routeNameData }.show`, item.id)"
+      class="inline-block pr-4 duration-100 rounded hover:text-green-600"
+    >
+      <slot>
+        <EyeIcon class="w-6 h-6" />
+      </slot>
+    </Link>
+
+
+    <div
+      v-else
+      class="inline-block pr-4 duration-100 rounded hover:text-green-600"
+    >
+      <slot>
+        <EyeIcon class="w-6 h-6" />
+      </slot>
+    </div>
+  </template>
 </template>
