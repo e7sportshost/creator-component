bf8ab5eca329f664ef73f83326b093dc6e699bce
cannon <p12576999@gmail.com>
2024-10-08 15:39:59 +0800

    a

--- DangerButton.vue
+++ DangerButton.vue
@@ -1,14 +1,19 @@
 <script setup>
+import { ElButton } from 'element-plus';
 defineProps({
     type: {
         type: String,
-        default: 'button',
+        default: 'submit',
+    },
+    size: {
+        type: String,
+        default: 'default',
     },
 });
 </script>
 
 <template>
-    <button :type="type" class="inline-flex items-center justify-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">
+    <ElButton color="#dc2626" class="mx-1 mb-4" type="danger" :native-type="type" :size="size">
         <slot />
-    </button>
+    </ElButton>
 </template>
